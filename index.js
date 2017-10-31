3edce69104c41c30125d87a7163fc5eb9e93d988
yoannlal <yoann.lal@gmail.com>
2017-10-31 16:22:19 +0100

    enable authorization header

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 // Set content type GLOBALLY for any response.
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Authorization");
   res.contentType('application/json');
   next();
 });
