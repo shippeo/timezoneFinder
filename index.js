9dd5558cf2f00510c2a0f4d67f3370439045e845
yoannlal <yoann.lal@gmail.com>
2017-10-31 15:31:53 +0100

    enable CORS

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = express();
 
 // Set content type GLOBALLY for any response.
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.contentType('application/json');
   next();
 });
